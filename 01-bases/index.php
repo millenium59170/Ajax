d9ed613318b2b12324e5b5f38dca09cc465e0674
Dekeuwer Laurent <millenium59170@gmail.com>
2019-03-11 11:03:54 +0100

    first commit

---
+++ 01-bases/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AJAX - Les bases</title>
+</head>
+<body>
+    <h1>Mon titre</h1>
+
+
+    <script> 
+    //on instancie le moteur AJAX
+    var xhr = new XMLHttpRequest();
+
+// On veut suivre l'évolution de la requête AJAX
+xhr.addEventListener('readystatechange', function () {
+    if (4 === xhr.readyState) {
+        if (200 === xhr.status) {
+            console.log(xhr.response);
+            document.getElementsByTagName('h1')[0].innerHTML = xhr.response;
+        }
+    }
+});
+
+// On prépare une requête HTTP
+xhr.open('GET', './worker.php');
+
+
+    //Executer une requete HTTP
+    xhr.open('GET', './worker.php');
+    xhr.send();
+    </script>
+</body>
+</html>
